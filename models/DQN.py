e61cfb5d8f0990dfde81d1d77b6f50f4fac1d500
Aark Koduru <aark@Aarks-MacBook-Pro.local>
2019-11-13 14:30:14 -0800

    fixed random sampling call

--- models/DQN.py
+++ models/DQN.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 import tqdm
 from tensorflow.keras import layers
 from tensorflow.keras.optimizers import RMSprop
-from collections import deque
+from collections import deque, namedtuple
 
 #seed
 np.random.seed(7)
@@ -28,12 +28,14 @@ target_policy_update = 0
 memory_size = 10_000
 episodes = 0
 
+#global variables
+SARS = namedtuple('Experience', ('state','action','reward','next_state'))
 env = gym.make("TrapTube-v0")
 observation = env.reset()
 action = env.action_space.sample()
 observation_next, reward, done, info = env.step(action)
 image = env.render(mode="rgb_array")  # also supports mode="human"
-print(env.action_space.sample())
+
 
 #Intrinsic curiosity module
 """
@@ -56,13 +58,50 @@ class DQN():
         model.compile(optimer=RMSprop(learning_rate=lr), loss='Huber', metrics=['accuracy'])
         return model
 
-class reply_memory():
+class ReplayMemory():
+    """
+    This is method returns a tuple which contains the state which the agent has been
+    to before, the action it took, the reward it got and the new state it reached
+    at that time
+    """
     def __init__(self,size):
         self.size = size
         self.memory = deque(maxlen=self.size)
 
     def update(self,SARS):
         self.memory.append(SARS)
+
+    def sample(self,batch_size):
+        return random.sample(self.memory,batch_size)
+
+class TrapTubeEnv():
+    def __init__(self):
+        self.env =  gym.make("TrapTube-v0")
+        self.env.reset()
+        self.curr_screen = None
+        self.done = False
+
+    def reset(self):
+        self.env.reset()
+        self.curr_screen = None
+
+    def close(self):
+        self.env.close()
+
+    def render(self,mode='rgb_array'):
+        return self.env.render(mode)
+
+    def actions_count(self):
+        return self.env.action_space.n
+
+    def take_action(self,action):
+        pass
+
+    def is_starting_state(self):
+        return self.curr_screen is None
+
+
+
 
 policy_net = DQN(image.shape[0],image.shape[1]).init_model()
 target_net = DQN(image.shape[0],image.shape[1]).init_model()
@@ -76,7 +115,7 @@ for episode in tqdm(range(1,episodes+1),unit ='episode'):
     while not done:
         #with probablity epsilon select a random action
         if random.random() < epsilon_start:
-            action = random.choice(env.action_space)
+            action = env.action_space.sample()
         else:
             #action = np.argmax(policy_net.predict(np.array()))
             pass
